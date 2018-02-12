94c8171528ebd5eb05a9cc086cd77a2efe1cfefa
Regaldisclaimer <regaldisclaimer@gmail.com>
2018-02-12 18:18:51 -0500

    add new version with material

--- old.js
+++ old.js
@@ -6,44 +6,44 @@ var screenPrintEl = document.querySelector("#screenPrint");
 //check webcam
 if (!!(
 		navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia
-	)) {} else {
+	)) {
+} else {
 	alert('Webcam Not supported in this browser');
 }
 
-var navBegin = function () {
+var navBegin = function() {
 	//hide start screen
 	screenStartEl.classList.toggle('hidden');
 	//get webcam
 	//chrome won't allow unless served over https
 	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-	navigator.getUserMedia({
-		video: true
-	}, function (localMediaStream) {
+	navigator.getUserMedia({video: true}, function(localMediaStream) {
 		var video = document.querySelector('video');
 		video.src = window.URL.createObjectURL(localMediaStream);
 
-		video.onloadedmetadata = function (e) {
+		video.onloadedmetadata = function(e) {
 			//ready to go. do some stuff.
 		};
-	}, function (e) {
+	}, function(e) {
 		alert("Webcam Error: ", e);
 	});
 	//turn on webcam screen
 	screenWebcamEl.classList.toggle('hidden');
 };
 
-var navSnap = function () {
+var navSnap = function() {
 
 };
 
-var navSubmit = function () {
+var navSubmit = function() {
 
 };
 
-var navPrint = function () {
+var navPrint = function() {
 
 };
 
-var navEnd = function () {
+var navEnd = function() {
 
 };
+
